a6c0f161212bf2e9c4beed82ff2cfb1512528d4f
lai2-24 <lai2-24@itismeucci.com>
2024-02-06 14:04:12 +0100

    added project files

---
+++ Alunno.php
@@ -0,0 +1,36 @@
+<?php
+class Alunno{
+    private $nome,$cognome, $eta;
+    public function __construct($nome,$cognome,$eta){
+        $this->nome = $nome;
+        $this->cognome = $cognome;
+        $this->eta = $eta;
+    }
+
+    public function GetNome(){
+        return $this->nome;
+    }
+    public function GetCognome(){
+        return $this->cognome;
+    }
+
+    public function GetEta(){
+        return $this->eta;
+    }
+
+    public function SetNome($nome){
+        $this->nome = $nome;
+    }
+    public function SetCognome($cogome){
+        $this->cognome = $cognome;
+    }
+
+    public function SetEta($eta){
+        $this->eta = $eta;
+    }
+
+    public function GetAll(){
+        return "Nome: " . $this->nome . "<br>Cognome: " . $this->cognome . "<br>Eta: " . $this->eta;
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    function __autoload($class_name){
+        require_once $class_name . '.php';
+    }
+
+    $arrayalunni = [new Alunno("marco","togni",27), new Alunno("mario","luigi",14), new Alunno("lorenzo","meucci",89)];
+    
+    for ($i=0; $i < sizeof($arrayalunni); $i++) { 
+        echo $arrayalunni[$i]->GetAll() . "<br><br>";
+    }
+    
+    
+    ?>
+</body>
+</html>
